6685a91ecdfc3e4b815695db5a96548c14b01777
maximilianofernandezlovey-rgb <maximilianofernandezlovey@gmail.com>
2026-08-21 09:27:32 -0300

    Agrega navegacion de regreso al header

--- components/AppHeader.tsx
+++ components/AppHeader.tsx
@@ -1,8 +1,12 @@
 import Link from "next/link";
+import BackButton from "./BackButton";
 
 export default function AppHeader(){
   return <header className="topbar">
-    <div className="brand">UADE</div>
+    <div style={{display:"flex",alignItems:"center",gap:14}}>
+      <div className="brand">UADE</div>
+      <BackButton/>
+    </div>
     <nav className="toplinks" aria-label="Navegación principal">
       <Link href="/gestion">Alumno</Link>
       <Link href="/operadores">Operadores</Link>
