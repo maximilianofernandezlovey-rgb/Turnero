5a4aee01f0786611883ab072abdd13a79ccd05a0
maximilianofernandezlovey-rgb <maximilianofernandezlovey@gmail.com>
2026-09-01 20:03:49 -0300

    Update box_date_scoping_repro.sql

--- tests/db/box_date_scoping_repro.sql
+++ tests/db/box_date_scoping_repro.sql
@@ -150,7 +150,6 @@ delete from public.turns where id in ('00000000-0000-4000-8000-000000000904','00
 delete from public.auth_sessions where id='00000000-0000-4000-8000-000000000802';
 delete from public.user_sector_memberships where user_id='00000000-0000-4000-8000-000000000801';
 delete from public.app_users where id='00000000-0000-4000-8000-000000000801';
-delete from public.service_points where id in ('00000000-0000-4000-8000-000000000302','00000000-0000-4000-8000-000000000303');
 
 -- limpieza de este test especifico
 delete from public.turns where id in (
@@ -158,3 +157,4 @@ delete from public.turns where id in (
   '00000000-0000-4000-8000-000000000902',
   '00000000-0000-4000-8000-000000000903'
 );
+delete from public.service_points where id in ('00000000-0000-4000-8000-000000000302','00000000-0000-4000-8000-000000000303');
